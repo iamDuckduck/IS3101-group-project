cfb957aacb6d2d626eb8f7f5798e80a01963c64e
iamDuckduck <125134849+iamDuckduck@users.noreply.github.com>
2025-03-29 15:24:44 +0800

    test: update test to handle msg.sender related test and new backers mapping

--- crowd_funding_test.sol
+++ crowd_funding_test.sol
@@ -24,6 +24,7 @@ contract CrowdFundingTest {
 
     /// ====== constructor test suit ====== /// 
 
+    /// Test initial owner success /// 
     function test_initial_owner_success() public {
         Assert.equal(crowdfunding.creator(), acc0, "init error on creator field");
         Assert.equal(crowdfunding.goal(), 1000, "init error on goal field");
@@ -32,6 +33,7 @@ contract CrowdFundingTest {
         Assert.equal(crowdfunding.description(), "test", "init error on description field");
     }
 
+    /// Test initial inital contract address fail /// 
     // not gonna test invalid goal, deadline, name and description
     // I did manually test them should be fine, but feel free to add these tests if you want
     function test_inital_contract_address_fail() public {
@@ -50,32 +52,28 @@ contract CrowdFundingTest {
 
     /// ====== update_campagin_status() test suit ====== ///
 
-    // Test updating campagin_status to success
+    /// Test updating campagin_status to success
     /// #value: 1000
-    /// #sender: account-1
     function test_update_campagin_status_success() public payable{
         crowdfunding.fund{value: 1000}(false);
         Assert.equal(uint(crowdfunding.campaignStatus()), 1, "Status mismatch");
     }
 
-    // Test updating campagin_status to fail
-
+    /// Test updating campagin_status to fail
     // manually test (pass)
     // I believe manually test is needed here since there is no way to adjust the time block (at least not in remix IDE)
-    
     
 
     /// ====== modifier campaignDeadlineCheck() test suit ====== ///
 
-    // Test fund after deadline (should fail)
-    // again manually test is needed (pass)
+    /// Test fund after deadline (should fail)
+    /// again manually test is needed (pass)
 
 
-    /// ====== fund() test suit ====== ///
+    /// ====== fund() test suite ====== ///
 
-    // Test zero funding (should fail)
+    /// Test zero funding (should fail)
     /// #value: 0
-    /// #sender: account-1
     function test_zero_funding() public payable {
         try crowdfunding.fund{value: 0}(false) {
             Assert.ok(false, "Should fail on zero fund");
@@ -86,7 +84,6 @@ contract CrowdFundingTest {
 
     /// Test flexiable funding (should succeed)
     /// #value: 10
-    /// #sender: account-1
     function test_flexiable_fund() public payable {
         try crowdfunding.fund{value: 10}(true) {
             Assert.ok(true, "Funding succeeded");
@@ -95,11 +92,11 @@ contract CrowdFundingTest {
         }
 
         Assert.equal(crowdfunding.totalFlexibleFunds(), 10, "Flexible Funds not match");
+        Assert.equal(crowdfunding.flexibleFundBalance(), 10, "flexibleFundBalance not match");
     }
 
     /// Test committed funding (should succeed)
     /// #value: 10
-    /// #sender: account-1
     function test_committed_fund() public payable {
         try crowdfunding.fund{value: 10}(false) {
             Assert.ok(true, "Funding succeeded");
@@ -113,7 +110,6 @@ contract CrowdFundingTest {
 
     /// Test flexible fund when campagin is successed (should fail)
     /// #value: 1001
-    /// #sender: account-1
     function test_flexible_fund_when_campagin_is_successed() public payable {
         crowdfunding.fund{value: 1000}(true);
 
@@ -129,7 +125,6 @@ contract CrowdFundingTest {
 
     /// Test overfunded (should success)
     /// #value: 1001
-    /// #sender: account-1
     function test_overfunded() public payable {
         crowdfunding.fund{value: 1000}(true);
 
@@ -139,24 +134,73 @@ contract CrowdFundingTest {
 
         Assert.equal(crowdfunding.totalCommittedFunds() + crowdfunding.totalFlexibleFunds(), 1001, "Total fund mismatch");
     }
-
-    /// ====== refund_flexiable_funds() test suit ====== ///
-    
-    // require manually test (pass)
-    // honesty i have no idea why it failed the test, manually test works perfect.
-
-    /// #value: 200
-    /// #sender: account-1
-    // function test_refund_flexiable_funds_before_deadline() public payable{
-    //     crowdfunding.fund{value: 200}(true);
-    //     crowdfunding.refund_flexiable_funds(200);
-    //     Assert.equal(crowdfunding.totalFlexibleFunds(), 0, "Total fund mismatch");
-    // }
-
-    /// ====== withdraw() test suit ====== ///
-
-    // require manually test (pass)
-
-   
 }
     
+
+//  have to inhertance the contract when testings involve msg.sender
+// otherwise we will just use this test contract to call the crowd_funding 
+// which mean we can't control the address of msg.sender when calling external function
+contract CrowdFundingTest2 is CrowdFunding(
+                                address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4), //acount-0
+                                1000,
+                                block.timestamp + 1000,
+                                "test",
+                                "test"
+                            ) {
+    
+    address acc0 = TestsAccounts.getAccount(0); 
+    address acc1 = TestsAccounts.getAccount(1);
+
+
+    /// ====== fund() test suite ====== ///
+
+    /// Test flexiable funding (should succeed)
+    /// #value: 10
+    /// #sender: account-1
+    function test_backer_flexibleFund() public payable {
+        fund(true);
+
+        (uint256 flexibleFund,) = this.backers(acc1);
+ 
+        Assert.equal(flexibleFund, 10, "Backer's Flexible Funds not match");
+    }
+
+    /// Test flexiable funding (should succeed)
+    /// #value: 10
+    /// #sender: account-1
+    function test_backer_committedFund() public payable {
+        fund(false);
+
+        (, uint256 committedFund) = this.backers(acc1);
+ 
+        Assert.equal(committedFund, 10, "Backer's committed Funds not match");
+    }
+
+    /// ====== refund_flexiable_funds() test suit ====== ///
+
+    /// Test refund_flexiable_funds (should succeed)
+    /// require manually test to check sender balance (pass)
+    /// #value: 200
+    /// #sender: account-1
+    function test_refund_flexiable_funds() public payable{
+        fund(true);
+        refund_flexiable_funds(210); // we fund 10 in previous test
+        Assert.equal(this.totalFlexibleFunds(), 0, "Total FlexibleFunds mismatch");
+        Assert.equal(this.flexibleFundBalance(), 0, "flexibleFundBalance not match");
+    }
+
+    /// ====== withdraw() test suit ====== ///
+    /// Test withdraw_when_success
+    /// require manually test (pass)
+    /// #value: 1000
+    /// #sender: account-0
+    function test_withdraw_when_success() public payable{
+        fund(true);
+        withdraw_remaining_funds();
+        Assert.equal(this.totalFlexibleFunds(), 1000, "Total FlexibleFunds mismatch");
+        Assert.equal(this.flexibleFundBalance(), 0, " flexibleFundBalance mismatch");
+    }
+
+  
+
+}
